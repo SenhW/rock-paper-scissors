82397ceb2890a8e2a462269609d60a5bda11ff9e
SenhW <senhwang3@gmail.com>
2014-12-25 02:10:28 -0500

    added winning and unbeaten streak stats

--- Game.java
+++ Game.java
@@ -6,6 +6,10 @@ public class Game {
     private int computerScore;
     private int playerScore;
     private int numGames;
+    private int winStreak;
+    private int unbeatenStreak;
+    private int maxWinStreak;
+    private int maxUnbeatenStreak;
 
     public enum Choice {
         ROCK, PAPER, SCISSORS
@@ -14,7 +18,7 @@ public class Game {
     public Choice playerChoice() {
         Character playerChoice;
         Scanner playerInput = new Scanner(System.in);
-        System.out.println("Enter \"r\" for rock, \"p\" for paper, or \"s\" for scissor");
+        System.out.println("Enter \"r\" for rock, \"p\" for paper, or \"s\" for scissor:");
         playerChoice = playerInput.next().charAt(0);
         switch(playerChoice) {
             case 'r': return Choice.ROCK;
@@ -50,21 +54,32 @@ public class Game {
             case -1:
                 System.out.println("Sorry, you lose.");
                 computerScore++;
+                winStreak = 0;
+                unbeatenStreak = 0;
                 break;
             case 0:
                 System.out.println("Tie!");
+                winStreak = 0;
+                unbeatenStreak++;
+                if(unbeatenStreak > maxUnbeatenStreak) maxUnbeatenStreak = unbeatenStreak;
                 break;
             case 1:
                 System.out.println("You win!");
                 playerScore++;
+                winStreak++;
+                unbeatenStreak++;
+                if(winStreak > maxWinStreak) maxWinStreak = winStreak;
+                if(unbeatenStreak > maxUnbeatenStreak) maxUnbeatenStreak = unbeatenStreak;
                 break;
         }
         numGames++;
         int ties = numGames - (playerScore + computerScore);
         // Calculate to 3 decimal places
         double percentage = (double)Math.round((playerScore + 0.5*ties) / numGames * 1000) / 1000;
-        System.out.println("Wins: " + playerScore + " Losses: " + computerScore + " Ties: " + ties +
-                " Wins Percentage: " + percentage);
+        System.out.println("Wins: " + playerScore + " Losses: " + computerScore + " Ties: " + ties + " Games Played: " +
+                numGames + " Wins Percentage: " + percentage);
+        System.out.println("Current win streak: " + winStreak + " Current unbeaten streak: " + unbeatenStreak);
+        System.out.println("Highest win streak: " + maxWinStreak + " Highest unbeaten streak: " + maxUnbeatenStreak);
     }
 
     public Character playAgain() {
